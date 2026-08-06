efa4ad12b68d380827461ea3c448c54307ce36bb
alshobatisalmanfares-cell <alshobatisalmanfares@gmail.com>
2026-08-06 18:32:07 +0300

    Update webDir path in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "app.lovable.zadalduat",
   appName: "زاد الدعاة",
-  webDir: "dist",
+  webDir: ".output/public",
 
   server: {
     androidScheme: "https"
