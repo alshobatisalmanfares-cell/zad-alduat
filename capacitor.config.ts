9052aa50b0a573652f2b8419abc1bb0ed0c82b2b
alshobatisalmanfares-cell <alshobatisalmanfares@gmail.com>
2026-08-06 18:27:31 +0300

    Change webDir from .output/public to dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 const config: CapacitorConfig = {
   appId: "app.lovable.zadalduat",
   appName: "زاد الدعاة",
-  webDir: ".output/public",
+  webDir: "dist",
 
   server: {
     androidScheme: "https"
